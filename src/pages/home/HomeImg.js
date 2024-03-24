4e566eb833aab857baebea923a1a8cb7f69e861f
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-03-24 14:32:44 +0530

    Update HomeImg.js

--- src/pages/home/HomeImg.js
+++ src/pages/home/HomeImg.js
@@ -1,5 +1,5 @@
 export const Img  = ["https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/5ecf4f27ed5376ef.jpg?q=20",
-
+"https://rukminim2.flixcart.com/fk-p-flap/1600/270/image/8644f8c9570972c6.jpg?q=20"
 "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/67bdb22c1fd23512.jpg?q=20",
 "https://rukminim1.flixcart.com/fk-p-flap/1600/270/image/8508f00d4e4eed20.jpg?q=20",
 "https://rukminim2.flixcart.com/fk-p-flap/1600/270/image/8403a0f8d48613a5.png?q=20"];
