419fe2c5ffff1a695bd0a414a0b8a681a4d29ac5
Mohammad Samir <87142605+Mdsamir92@users.noreply.github.com>
2024-05-21 12:27:46 +0530

    Update Search.js

--- src/components/Searchbar/Search.js
+++ src/components/Searchbar/Search.js
@@ -2,6 +2,8 @@ import {  useState,useContext } from "react";
 import {Link, useNavigate } from "react-router-dom";
 import MyContext from '../../context/Context';
 import { useSelector } from "react-redux";
+import {IoMdClose} from "react-icons/io";
+import { BsSearch } from 'react-icons/bs';
 
 
 const Search = () => {
@@ -19,7 +21,9 @@ const Search = () => {
     // Filter Search Data
     const filterSearchData = getAllProduct.filter((obj) => obj.title.toLowerCase().includes(search)).slice(0, 8)
 
-
+    const handleClose = () => {
+        setSearch("");
+    }
 
     return (
         <div className="">
@@ -33,13 +37,21 @@ const Search = () => {
                 </Link>
                 </li>
             </ul>
-                <input
+            <div className="relative w-52 lg:w-50 md:w-50 outline-none">
+            <input
                     type="text"
                     value={search}
                     placeholder='Search here...'
                     onChange={(e) => setSearch(e.target.value)}
-                    className=' bg-gray-200 placeholder-gray-400 rounded-full px-2 py-2 w-50 lg:w-50 md:w-50 outline-none text-black '
+                    className=' bg-gray-200 placeholder-gray-400 rounded-full relative  px-2 py-2 w-52 lg:w-50 md:w-50 outline-none text-black '
                 />
+                 {
+                        search === "" ? (<BsSearch className='cursor-pointer absolute right-0 top-0 mr-4 mt-3 text-gray-400' size={20} />
+                        ) : (<IoMdClose className='cursor-pointer absolute right-0 top-0 mr-3 mt-3 text-gray-400' size={20} onClick={handleClose} />)
+                    } 
+            </div>
+                  
+            
             </div>
 
             {/* search drop-down  */}
@@ -47,12 +59,13 @@ const Search = () => {
                 {search && <div className="block absolute bg-gray-200 w-50 md:w-50 lg:w-40 z-50 my-1 rounded-lg px-2 py-1">
                     {filterSearchData.length > 0 ?
                         <>
+                        <IoMdClose className='cursor-pointer absolute right-0 top-0 mx-2 mt-2 text-gray-400' size={20} onClick={handleClose} />
                             {filterSearchData.map((item, index) => {
                                 return (
                                     <div  onClick={()=> navigate(`/productinfo/${item.id}`)} key={index} className="py-2 px-2 cursor-pointer">
                                         <div className="flex items-center gap-2">
                                             <img className="w-10" src={item.productImageUrl} alt="" />
-                                            {item.title}
+                                            <p className='text-xs'> {item.title.slice(0, 20)}</p>
                                         </div>
                                     </div>
                                 )
